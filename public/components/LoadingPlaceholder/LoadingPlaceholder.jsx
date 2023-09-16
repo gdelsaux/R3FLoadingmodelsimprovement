49c53b30a7e2584da78eb7184f125e4a5ff30059
gaetan <gaetan.delsaux@room58.com>
2023-09-16 12:45:54 +0100

    removed unnecessary tags

--- public/components/LoadingPlaceholder/LoadingPlaceholder.jsx
+++ public/components/LoadingPlaceholder/LoadingPlaceholder.jsx
@@ -5,10 +5,8 @@ export default function LoadingPlaceholder({position, scale, args, color}) {
         y: 0,
         z: 0,
     }
-    return <>
-        <mesh position-y={ modelPosition.y } scale={ modelScale }>
-            <boxGeometry args={ args } />
-            <meshBasicMaterial wireframe color={color} />
-        </mesh>
-    </>
+    return <mesh position-y={ modelPosition.y } scale={ modelScale }>
+        <boxGeometry args={ args } />
+        <meshBasicMaterial wireframe color={color} />
+    </mesh>
 }
